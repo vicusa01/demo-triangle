832a958fb5e673a6e5d1dbcd2c9d3376f7021672
viktoriya-tsygler-idf <viktoriya.tsygler@idftechnology.by>
2025-01-07 22:55:10 +0300

    add method defineTriangle to class Triangles

--- src/main/java/by/itacademy/tsyhler/Triangles.java
+++ src/main/java/by/itacademy/tsyhler/Triangles.java
@@ -1,5 +1,19 @@
 package by.itacademy.tsyhler;
 
 public class Triangles {
+    public static String defineTriangle(int a, int b, int c) {
+        String result = "Треугольник a=" + a + ", b=" + b + ", c=" + c;
+        if (a + b <= c || a + c <= b || c + b <= a || a <= 0 || b <= 0 || c <= 0)
+            result += " - не является треугольником";
+        else if (b == c && a == c)
+            result += " - равносторонний треугольник";
+        else if (a == b || b == c || a == c) {
+            result += " - равнобедренный треугольник";
+        } else if (a * a + b * b == c * c || b * b + c * c == a * a || a * a + c * c == b * b)
+            result += " - прямоугольный треугольник";
+        else
+            result += " - обычный треугольник";
 
+        return result;
+    }
 }
